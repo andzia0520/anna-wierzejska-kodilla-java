2e10464a281fc9f9d3c556638278749996453882
andzia0520 <ankadaniluk@gmail.com>
2020-06-12 16:59:06 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addingResult = calculator.addAToB();
+        if(addingResult == 38) {
+            System.out.println("Addingtest OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        int subtractResult = calculator.subtractBFromA();
+        if(subtractResult == 12) {
+            System.out.println("Subtractest OK");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    int a = 25;
+    int b = 13;
+
+    public int addAToB() {
+        return a + b;
+    }
+
+    public int subtractBFromA() {
+        return a - b;
+    }
+}
