3e29e365a6aafd896c312fc105f3dfdaee81d54b
andzia0520 <ankadaniluk@gmail.com>
2020-06-24 11:16:52 +0200

    example modul 7.2.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -11,5 +12,9 @@ public class StreamMain {
         poemBeautifier.beautify("Maybe JaVa iS not sO difficulT.", text -> text.toLowerCase());
         poemBeautifier.beautify("   It is...   ", text -> text.trim());
         poemBeautifier.beautify("Task done?", text -> "* " + text + " *");
+
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
