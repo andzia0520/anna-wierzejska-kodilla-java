a92057acaa1765f62fd0bca4081d01864440d53b
andzia0520 <ankadaniluk@gmail.com>
2020-06-24 10:05:28 +0200

    7.1. version 2.0.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,16 +2,14 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 
-import static jdk.nashorn.internal.objects.NativeString.*;
-
 public class StreamMain {
     public static void main(String[] args) {
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
-        poemBeautifier.beautify("ThIs iS mOdUl SeVeN.", (text) -> toUpperCase(text));
-        poemBeautifier.beautify("Maybe JaVa iS not sO difficulT.", (text) -> toLowerCase(text));
-        poemBeautifier.beautify("   It is...   ", (text) -> trim(text));
-        poemBeautifier.beautify("Task done?", (text) -> "* " + text + " *");
+        poemBeautifier.beautify("ThIs iS mOdUl SeVeN.", text -> text.toUpperCase());
+        poemBeautifier.beautify("Maybe JaVa iS not sO difficulT.", text -> text.toLowerCase());
+        poemBeautifier.beautify("   It is...   ", text -> text.trim());
+        poemBeautifier.beautify("Task done?", text -> "* " + text + " *");
     }
 }
