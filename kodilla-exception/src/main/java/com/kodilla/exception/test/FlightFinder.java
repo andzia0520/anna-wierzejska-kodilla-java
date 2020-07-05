c9b6850306ed787c69f0b39036bca694a0488dc2
andzia0520 <ankadaniluk@gmail.com>
2020-07-05 23:37:01 +0200

    task 8.4.

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,44 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightFinder {
+
+    public void findFlight(Flight flight) throws RouteNotFoundException {
+
+        Map<String, Boolean> flightsNetwork = new HashMap<>();
+        flightsNetwork.put("Warsaw", true);
+        flightsNetwork.put("Barcelona", false);
+        flightsNetwork.put("Madrid", true);
+        flightsNetwork.put("Budapest", false);
+
+
+        for (Map.Entry<String, Boolean> entry : flightsNetwork.entrySet()) {
+            if (entry.getValue() == false) {
+                throw new RouteNotFoundException("Your destination airport doesn't exist.");
+            } else {
+                System.out.println("The flight from " + flight.getDepartureAirport() + " to " + flight.getArrivalAirport() + " is available.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Flight flight = new Flight("Moscow", "Warsaw");
+        FlightFinder flightFinder = new FlightFinder();
+
+        try {
+
+            flightFinder.findFlight(flight);
+
+        } catch (RouteNotFoundException e) {
+
+            System.out.println("Your destination airport doesn't exist.");
+
+        } finally {
+
+            System.out.println("Thank you for using our flights services. See you again !");
+        }
+    }
+}
