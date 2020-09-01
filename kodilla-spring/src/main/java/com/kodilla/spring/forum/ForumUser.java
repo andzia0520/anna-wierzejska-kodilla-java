65b95e043ec6f875e8e964f5c2d0255dac5f520a
andzia0520 <ankadaniluk@gmail.com>
2020-09-01 21:00:45 +0200

    task 14.1. with private final

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ForumUser {
-    String userName;
+    private final String userName;
 
     public ForumUser() {
         this.userName = "John Smith";
