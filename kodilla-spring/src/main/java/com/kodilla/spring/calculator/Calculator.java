f9cd5e2fa6d9a166c4686a15df0a5d65f0a6b9bc
andzia0520 <ankadaniluk@gmail.com>
2020-09-02 21:01:35 +0200

    little correction task 14.2.

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,6 +1,5 @@
-package com.kodilla.spring.library;
+package com.kodilla.spring.calculator;
 
-import com.kodilla.spring.calculator.Display;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,6 +1,5 @@
 package com.kodilla.spring.calculator;
 
-import com.kodilla.spring.library.Calculator;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
