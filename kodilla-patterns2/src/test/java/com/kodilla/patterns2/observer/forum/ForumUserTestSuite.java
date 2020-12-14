be65510c4abebdc28d2db36cc51f7e2b55fa9ecb
andzia0520 <ankadaniluk@gmail.com>
2020-12-14 14:05:08 +0100

    modul 24.4. example

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/forum/ForumUserTestSuite.java
@@ -0,0 +1,33 @@
+package com.kodilla.patterns2.observer.forum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ForumUserTestSuite {
+    @Test
+    public void testUpdate() {
+        //Given
+        ForumTopic javaHelpForum = new JavaHelpForumTopic();
+        ForumTopic javaToolsForum = new JavaToolsForumTopic();
+        ForumUser johnSmith = new ForumUser("John Smith");
+        ForumUser ivoneEscobar = new ForumUser("Ivone Escobar");
+        ForumUser jessiePinkman = new ForumUser("Jessie Pinkman");
+        javaHelpForum.registerObserver(johnSmith);
+        javaToolsForum.registerObserver(ivoneEscobar);
+        javaHelpForum.registerObserver(jessiePinkman);
+        javaToolsForum.registerObserver(jessiePinkman);
+
+        //When
+        javaHelpForum.addPost("Hi everyone, could you help with loop");
+        javaHelpForum.addPost("Better try with while loop");
+        javaToolsForum.addPost("Help, sql doesnt work !!!");
+        javaHelpForum.addPost("Why");
+        javaToolsForum.addPost("I can't log in");
+
+        //Then
+        assertEquals(3, johnSmith.getUpdateCount());
+        assertEquals(2, ivoneEscobar.getUpdateCount());
+        assertEquals(5, jessiePinkman.getUpdateCount());
+    }
+}
