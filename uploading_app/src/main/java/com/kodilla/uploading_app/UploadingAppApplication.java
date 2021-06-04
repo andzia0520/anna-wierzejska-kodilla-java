0d3a947c51e083aa5e445dae77afaaa8d3eb6476
andzia0520 <ankadaniluk@gmail.com>
2021-06-04 15:18:17 +0200

    first settings

---
+++ uploading_app/src/main/java/com/kodilla/uploading_app/UploadingAppApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.uploading_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadingAppApplication.class, args);
+    }
+
+}
---
+++ uploading_app/src/test/java/com/kodilla/uploading_app/UploadingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.uploading_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
